72592b2f4630f96afaa01c0e9106630ae3d87622
Andgonza09 <andresgonza091006@gmail.com>
2026-09-15 21:31:38 -0600

    prueba de cambios

--- View/frmBeta.Designer.cs
+++ View/frmBeta.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(96, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 51);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // frmBeta
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
             Name = "frmBeta";
             Text = "frmBeta";
             Load += frmBeta_Load;
@@ -42,5 +53,7 @@
         }
 
         #endregion
+
+        private Button button1;
     }
 }
